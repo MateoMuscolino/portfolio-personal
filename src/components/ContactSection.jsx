7e4226ac00bcf6ee2c3c6ea5c2f27b122820e308
Mateo Muscolino <muscolinomateo@gmail.com>
2025-08-05 12:44:32 -0300

    links added to href social icons

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -88,10 +88,10 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a href="https://www.linkedin.com/in/mateo-muscolino-b8aab0234/" target="_blank">
                   <Linkedin />
                 </a>
-                <a href="#" target="_blank">
+                <a href="https://www.instagram.com/mateomuscolino/" target="_blank">
                   <Instagram />
                 </a>
               </div>
